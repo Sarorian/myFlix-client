31e872ce56873a616da854d33b9784ed70ba8b15
Sarorian <shamowen5@gmail.com>
2023-04-28 00:48:08 -0600

    started working with react

---
+++ src/components/movie-card/movie-view/movie-view.jsx
@@ -0,0 +1,27 @@
+export const MovieView = ({ movie, onBackClick }) => {
+    return (
+      <div>
+        <div>
+          <img src={movie.image} />
+        </div>
+        <div>
+          <span>Title: </span>
+          <span>{movie.title}</span>
+        </div>
+        <div>
+          <span>Description: </span>
+          <span>{movie.description}</span>
+        </div>
+        <div>
+          <span>Directed by: </span>
+          <span>{movie.director}</span>
+        </div>
+        <div>
+          <span>Genre: </span>
+          <span>{movie.genre}</span>
+        </div>
+        <button onClick={onBackClick}>Back</button>
+      </div>
+    );
+  };
+  
